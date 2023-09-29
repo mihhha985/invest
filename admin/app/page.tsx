bedc9d9ad9e7d368b5647d8da4ea2b47753467c7
Mikhail Biketov <mihhha985@yandex.ru>
2023-09-29 10:12:45 +0300

    add admin app

---
+++ admin/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-slate-50">
+      <h1 className="text-zinc-600 text-6xl font-bold">ADMIN APP</h1>
+    </div>
+  )
+}
